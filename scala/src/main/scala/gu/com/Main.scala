66969b64cfd9a854584acc2c5c7ec09dad73c39d
Maria Olanipekun <m.olanipekun@hotmail.com>
2021-11-26 13:33:32 +0000

    wip - calculate percentage

--- scala/src/main/scala/gu/com/Main.scala
+++ scala/src/main/scala/gu/com/Main.scala
@@ -24,14 +24,15 @@ package gu.com
 
 case class PartyResult(votes: Int, partyCode: String)
 
-case class ConstituencyResult(constituencyName: String, results: List[PartyResult])
+case class ConstituencyResult(
+    constituencyName: String,
+    results: List[PartyResult]
+)
 
-case class PartyShare(partyCode: String, percentage: Double)
+case class PartyShare(partyCode: String, percentage: Float)
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    
-  }
+  def main(args: Array[String]): Unit = {}
   def whereWeLive = "Earth"
 
   def extractResults(constituencyVotes: String): ConstituencyResult = {
@@ -39,24 +40,28 @@ object Main {
     val list = constituencyVotes.split(", ").toList
     val constituencyName = list.head
     val results = list.tail
-    val partyResults: List[PartyResult] = results.grouped(2).toList.map(r => PartyResult(r.head.toInt, r.last))
+    val partyResults: List[PartyResult] =
+      results.grouped(2).toList.map(r => PartyResult(r.head.toInt, r.last))
     ConstituencyResult(constituencyName, partyResults)
   }
 
   def convertPartyCode(partyCode: String): String = partyCode match {
-    case "C" => "Conservative Party"
-    case "L" => "Labour Party"
+    case "C"    => "Conservative Party"
+    case "L"    => "Labour Party"
     case "UKIP" => "UKIP"
-    case "LD" => "Liberal Democrats"
-    case "G" => "Green Party"
-    case "Ind" => "Independent"
-    case "SNP" => "SNP"
-    case _ => "Error"
+    case "LD"   => "Liberal Democrats"
+    case "G"    => "Green Party"
+    case "Ind"  => "Independent"
+    case "SNP"  => "SNP"
+    case _      => "Error"
   }
 
   def calculatePercentage(partyResults: List[PartyResult]): List[PartyShare] = {
     // List(PartyResult(25, "C"), PartyResult(75, "L"))
     val total = partyResults.map(r => r.votes).sum
-    partyResults.map(r => PartyShare(r.partyCode, r.votes / total))
+    partyResults.map(r => {
+      val percentageCalc = ((r.votes).toFloat / total) * 100
+      PartyShare(r.partyCode, percentageCalc)
+    })
   }
 }
